24fbb772239c2fe290871674586129230949c266
AhmedHaj <ahmed1998nidal@gmail.com>
2018-09-04 23:48:10 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def hello
-    render html: "Hello, World!"
-  end
-  
-  def thanks
-    render html: "You have my thanks!"
-  end
 end
